d9fa48b87c8263ecd0e622f3838d52c565daec99
James Murphy <james@mcoding.io>
2021-09-08 16:07:54 -0500

    see what it looks like to fail workflow

--- tests/test_slapping.py
+++ tests/test_slapping.py
@@ -29,7 +29,7 @@ def test_regex_slaps():
     assert slap_many(LikeState.empty, '[ld]*ddl') is LikeState.liked
 
 
-@pytest.mark.xfail
+# @pytest.mark.xfail
 def test_divide_by_zero():
     assert 1 / 0 == 1
 
